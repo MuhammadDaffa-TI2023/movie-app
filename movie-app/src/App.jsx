b7ba9835daa4c0165648cff740910051c060c001
Muhammad Daffa <daffaaja530@gmail.com>
2025-03-19 13:34:12 +0700

    movie-app

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from './components/hello'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h2>This is create react app</h2>
+      <Hello name="daffa"/>
+    </div>
+  )
+
+}
+
+export default App
